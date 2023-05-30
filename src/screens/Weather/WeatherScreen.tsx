3bee6a2f4b1e9cd820a25b8a3b1f294361e66bc2
maynard <33montefalconyoyon@gmail.com>
2023-05-30 21:14:07 +0800

    Implement fetching of weather forecast per city

--- src/screens/Weather/WeatherScreen.tsx
+++ src/screens/Weather/WeatherScreen.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import {useNavigate, useParams} from "react-router-dom";
-import {Box, Button, Container} from "@mui/material";
+import {Box, Button, Container, Grid} from "@mui/material";
 import axios from "axios";
 import {useEffect, useState} from "react";
 import {
@@ -10,57 +10,72 @@ import {
     TableHead,
     TableRow
 } from "@mui/material";
+import moment from "moment";
 
-type WeatherScreenProps = {
-    [key: string]: any;
-}
+const displayNoResultsFound = () => {
+    return (
+        <TableContainer component={Paper}>
+            <Table aria-label="simple table">
+                <TableBody>
+                    <TableRow>
+                        <TableCell className="span-body" align={"center"} style={{width: '20%'}}
+                                   sx={{fontFamily: "Roboto, Helvetica, Arial, sans-serif"}}>
+                            Results not found.
+                        </TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+};
 
 // Function that will display the weather based on the argument
-const displayWeather = (weatherDetails): JSX.Element => {
+const displayWeather = weatherDetails => {
+    if (!weatherDetails) return displayNoResultsFound();
+
+    // Convert unix to timestamp
+    const timestampObj = moment.unix(weatherDetails.dt);
+    const result = timestampObj.format("MM/DD/YYYY")
+
     return (
         <>
             {
-                Object.keys(weatherDetails).length < 1 ?
-                    <TableCell className="span-body" align={"center"} style={{width: '20%'}}
-                               sx={{fontFamily: "Roboto, Helvetica, Arial, sans-serif"}}>
-                        Results not found.
-                    </TableCell> :
-                    <TableContainer component={Paper}>
-                        <Table aria-label="simple table">
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell>Date (mm/dd/yyyy)</TableCell>
-                                    <TableCell>Temp(F)</TableCell>
-                                    <TableCell>Description</TableCell>
-                                    <TableCell>Main</TableCell>
-                                    <TableCell>Pressure</TableCell>
-                                    <TableCell>Humidity</TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                <TableRow>
-                                    <TableCell component="th" scope="row">
-                                        {weatherDetails.date}
-                                    </TableCell>
-                                    <TableCell component="th" scope="row">
-                                        {weatherDetails.temp}
-                                    </TableCell>
-                                    <TableCell component="th" scope="row">
-                                        {weatherDetails.description}
-                                    </TableCell>
-                                    <TableCell component="th" scope="row">
-                                        {weatherDetails.main}
-                                    </TableCell>
-                                    <TableCell component="th" scope="row">
-                                        {weatherDetails.pressure}
-                                    </TableCell>
-                                    <TableCell component="th" scope="row">
-                                        {weatherDetails.humidity}
-                                    </TableCell>
-                                </TableRow>
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
+                <TableContainer component={Paper}>
+                    <Table aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Date (mm/dd/yyyy)</TableCell>
+                                <TableCell>Temp(F)</TableCell>
+                                <TableCell>Description</TableCell>
+                                <TableCell>Main</TableCell>
+                                <TableCell>Pressure</TableCell>
+                                <TableCell>Humidity</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell component="th" scope="row">
+                                    {result}
+                                </TableCell>
+                                <TableCell component="th" scope="row">
+                                    {weatherDetails.main.temp}
+                                </TableCell>
+                                <TableCell component="th" scope="row">
+                                    {weatherDetails.weather[0].description}
+                                </TableCell>
+                                <TableCell component="th" scope="row">
+                                    {weatherDetails.weather[0].main}
+                                </TableCell>
+                                <TableCell component="th" scope="row">
+                                    {weatherDetails.main.pressure}
+                                </TableCell>
+                                <TableCell component="th" scope="row">
+                                    {weatherDetails.main.humidity}
+                                </TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </TableContainer>
             }
         </>
     );
@@ -74,10 +89,11 @@ const WeatherScreen = () => {
     useEffect(() => {
         const fetchWeather = async () => {
             try {
-                let appId: string = "b1aab4e616aa07c3e8a48526923482ac";
-                let url: string = `https://pro.openweathermap.org/data/2.5/forecast/hourly?q=${routeParam.id}&appid=${appId}`;
+                let appId: string = "5185503dca19cef88202028baf9f40ed";
+                let url: string = `https://api.openweathermap.org/data/2.5/weather?q=${routeParam.id}&appid=${appId}`;
                 const weatherResult = await axios.get(url);
-                console.log(weatherResult)
+                const {data} = weatherResult;
+                setWeatherDetails(data);
             } catch (e) {
                 console.log(e)
             }
@@ -86,14 +102,14 @@ const WeatherScreen = () => {
         fetchWeather();
     }, []);
 
-
     return (
         <Container component="main" maxWidth="s" sx={{marginTop: "80px"}}>
-            <Box sx={{textAlign: "center"}}>
+            <Box>
                 {
-                    displayWeather({date: "09/01/2020", temp: 75, description: "Sky is clear", main: "Clear", pressure: 1023.68, humidity: "100"})
+                    displayWeather(weatherDetails)
                 }
-                <Button type="submit" align={"right"} variant="contained" sx={{mt: 3, mb: 2}} onClick={() => navigate('/dashboard')}>
+                <Button type="submit" variant="contained" sx={{mt: 3, mb: 2, float: "right"}}
+                        onClick={() => navigate('/dashboard')}>
                     Back
                 </Button>
             </Box>
